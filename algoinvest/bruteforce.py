a0ad33517f9bf9bbff07383945cfda9a39f4743b
johnchem <j.payssan@gmail.com>
2022-08-30 11:23:41 +0200

    unitest finalisation & brute force creation

--- algoinvest/bruteforce.py
+++ algoinvest/bruteforce.py
@@ -1,6 +1,29 @@
 
-def brute_force():
-    pass
+from collections import namedtuple
+import copy
+from email.policy import default
+
+def brute_force(market):
+    list_investment = [{'shares':[], 'cost':0, 'roi':0}]
+    for share in market:
+        list_investment = add_share(share, list_investment)
+    return list_investment
+
+def add_share(share, investment):
+    output = []
+    for invest in investment:
+        choice_1, choice_2 = copy.deepcopy(invest), copy.deepcopy(invest)
+        
+        choice_1["shares"].append(share[0])
+        choice_1["cost"] += int(share[1])
+        choice_1["roi"] += round(float(share[1]*share[2]/100), 2)
+
+        choice_2["shares"].append("")
+        output.append(choice_1)
+        output.append(choice_2)
+    return output
 
 if __name__ == '__main__':
-    pass
+    test = [['A',1, 1], ['B',1, 1], ['C',1, 1], ['D',1, 1], ['E',1, 1], ['F',1, 1]]
+    test_output = brute_force(test)
+    print(*test_output, sep="\n")
--- algoinvest/unitest.py
+++ algoinvest/unitest.py
@@ -1,53 +1,94 @@
-from collections import namedtuple
 from bruteforce import brute_force
 from optimized import optimized
+
 from pathlib import Path
+from collections import namedtuple
+import pytest
 import csv
 
-def test_bruteforce_set_1():
-    DATA_SET_FOLDER = Path("algoinvest/dataset")
-    control_set_1 = import_control_set_1(DATA_SET_FOLDER/'control_dataset1.csv')
-    testing_set_1 = import_dataset(DATA_SET_FOLDER/'dataset1_Python+P7.csv')
+#define testing constant
+DATA_SET_FOLDER = Path("algoinvest/dataset")
+
+def test_bruteforce_set_1(dataset_1, control_set_1):
+    shares, cost, roi = brute_force(dataset_1)
+
+    assert all(share in dataset_1 for share in shares)
+    assert cost <= control_set_1['cost']
+    assert roi >= control_set_1['roi']
+
+
+def test_bruteforce_set_2(dataset_2, control_set_2):
+    shares, cost, roi = brute_force(dataset_2)
+
+    assert all(share in dataset_2 for share in shares)
+    assert cost <= control_set_2['cost']
+    assert roi >= control_set_2['roi']
+
+def test_optimized_set_1(dataset_1, control_set_1):
+    shares, cost, roi = optimized(dataset_1)
     
-def test_bruteforce_set_2():
-    DATA_SET_FOLDER = Path("algoinvest/dataset")
-    control_set_2 = import_control_set_2(DATA_SET_FOLDER/'control_dataset2.csv')
-    testing_set_2 = import_dataset(DATA_SET_FOLDER/'dataset2_Python+P7.csv')
-
-def test_optimized_set_1():
-    DATA_SET_FOLDER = Path("algoinvest/dataset")
-    control_set_1 = import_control_set_1(DATA_SET_FOLDER/'control_dataset1.csv')
-    testing_set_1 = import_dataset(DATA_SET_FOLDER/'dataset1_Python+P7.csv')
-
-def test_optimized_set_2():
-    DATA_SET_FOLDER = Path("algoinvest/dataset")
-    control_set_2 = import_control_set_2(DATA_SET_FOLDER/'control_dataset2.csv')
-    testing_set_2 = import_dataset(DATA_SET_FOLDER/'dataset2_Python+P7.csv')
+    assert all(share in dataset_1 for share in shares)
+    assert cost <= control_set_1['cost']
+    assert roi >= control_set_1['roi']
 
 
-def import_dataset(fileName):
+def test_optimized_set_2(dataset_2, control_set_2):
+    shares, cost, roi = optimized(dataset_2)
+
+    assert all(share in dataset_2 for share in shares)
+    assert cost <= control_set_2['cost']
+    assert roi >= control_set_2['roi']
+
+
+"""
+define the fixture
+"""
+@pytest.fixture
+def dataset_1():
+    FILE_NAME = DATA_SET_FOLDER/'dataset1_Python+P7.csv'
+    
     share = namedtuple('share',('value', 'roi'))
-    with open(fileName, newline='') as f:
+    with open(FILE_NAME, newline='') as f:
         reader = csv.reader(f, delimiter= ',')
         next(reader) #skip the 1st line
         control_data = {key:share(value=value,roi=roi) for key, value, roi in reader}
     return control_data
 
 
-def import_control_set_1(fileName):
-    with open(fileName, newline='') as f:
-        print('tutu')
+@pytest.fixture
+def dataset_2():
+    FILE_NAME = DATA_SET_FOLDER/'dataset2_Python+P7.csv'
+    
+    share = namedtuple('share',('value', 'roi'))
+    with open(FILE_NAME, newline='') as f:
         reader = csv.reader(f, delimiter= ',')
-        control_data = {key.strip():value.strip() for key, value in reader}
-    del control_data['Sienna bought:']
+        next(reader) #skip the 1st line
+        control_data = {key:share(value=value,roi=roi) for key, value, roi in reader}
     return control_data
 
 
-def import_control_set_2(fileName):
-    with open(fileName, newline='') as f:
+@pytest.fixture
+def control_set_1():
+    FILE_NAME = DATA_SET_FOLDER/'control_dataset1.csv'
+
+    with open(FILE_NAME, newline='') as f:
         reader = csv.reader(f, delimiter= ',')
         control_data = {key.strip():value.strip() for key, value in reader}
     del control_data['Sienna bought:']
+    control_data['cost'] = control_data.pop('Total cost:')
+    control_data['roi'] = control_data.pop('Total return:')
+    return control_data
+
+@pytest.fixture
+def control_set_2():
+    FILE_NAME = DATA_SET_FOLDER/'control_dataset2.csv'
+
+    with open(FILE_NAME, newline='') as f:
+        reader = csv.reader(f, delimiter= ',')
+        control_data = {key.strip():value.strip() for key, value in reader}
+    del control_data['Sienna bought:']
+    control_data['cost'] = control_data.pop('Total cost:')
+    control_data['roi'] = control_data.pop('Profit:')
     return control_data
 
 
